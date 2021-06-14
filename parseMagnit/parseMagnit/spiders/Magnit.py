636a25b9827fe7858b983fcd165619da68bd1d36
PavelChech <asepush@gmail.com>
2021-06-14 04:11:44 +0300

    Реализован парсинг необходимых элементов, кроме отдельно догружающихся javascript'ом

--- parseMagnit/parseMagnit/spiders/Magnit.py
+++ parseMagnit/parseMagnit/spiders/Magnit.py
@@ -1,5 +1,8 @@
 import scrapy
 import datetime
+import time
+import json 
+
 
 class MagnitSpider(scrapy.Spider):
     name = 'Magnit'
@@ -16,12 +19,9 @@ class MagnitSpider(scrapy.Spider):
     def parse(self, response):
         pageCount = int(response.xpath("//div[@class='pageCount']/text()").extract_first())
         curPage = int(response.xpath("//div[@class = 'curPage']/text()").extract_first())
-        # print("----------------------Parse--------------------------")
-        # print(curPage, pageCount)
 
         for href in response.xpath("//a[contains(@class, 'product__link')]/@href").extract():
             url_page = 'https://' + self.allowed_domains[0] + href
-            # print(url_page)
             yield scrapy.Request(url_page, callback=self.parsePage)
         
         quotes_base_url = response.urljoin('?PAGEN_1=%s')
@@ -31,32 +31,41 @@ class MagnitSpider(scrapy.Spider):
         
 
     def parsePage(self, response):
-        timestamp = datetime.datetime.now().timestamp()
         
-        # print("----------------------parsePage--------------------------")
+        result = {"timestamp": datetime.datetime.now().timestamp()}
         
-        
-        result = {
-            "timestamp": timestamp,
-            # "RPC": RPC,
-            # "color": color,
-            # "url": url,
-            # "title" : title,
-            # "marketing tags": marketing_tags,
-            # "brand": brand,
-            # "section": section,
-            # "price data": {"current": current_price,
-            #               "original": original_price,
-            #               "sale_tag": sale_tag},
-            # "stock":{"in stock": in_stock,
-            #          "count": 0},
-            # "assets": {"main image": main_image,
-            #            "set images": set_images,
-            #            "view360": view,
-            #            "video": video},
-            # "metadata": {"description": description,
-            #              "metadata": metadata},
-            # "variants": variants
-        }
+        result |= {"RPC": response.url.split('/')[-2]}
+
+        result |= {"title": MagnitSpider.clearStr(response.xpath("//h1[@class = 'action-card__name']/text()").extract_first())}
+
+        result |= {"marketing tags": response.xpath("//span[@class = 'event__product-title']/text()").extract()}
+
+        result |= {"brand": response.xpath("//td[@class = 'action-card__cell']/text()").extract()[1].strip('\n\t').strip()}
+
+        section = response.xpath("//a[@class = 'breadcrumbs__link']/text()").extract()
+        for i in range(len(section)):
+            section[i] = MagnitSpider.clearStr(section[i])
+        result |= {"section": section}
+
+        assets = {"main_image": 'https://' + self.allowed_domains[0] + response.xpath("//img[@class = 'product__image']/@src").extract_first(),
+                    "set_images": [],
+                    "view360": [], 
+                    "video": []}  
+        result |= {"assets": assets}
+
+        metadata = {"description": response.xpath("//div[@class = 'action-card__text']/text()").extract_first()}
+        item_list = response.xpath("//td[@class = 'action-card__cell']/text()").extract()[2:]
+        for i in range(0, len(item_list), 2):
+            metadata |= {MagnitSpider.clearStr(item_list[i])[:-1]: MagnitSpider.clearStr(item_list[i+1])}
+        result |= metadata
+
+        result |= {"variant": 1}
 
         yield result
+
+    # def parseEvent(self, response):
+    #     pass
+
+    @staticmethod
+    def clearStr(str):
+        return str.strip('\n\t').strip()
